56000137e69b3caf2f299abca17a9ca26d8f5013
bdrailwaykotchandpur <bdrailwaykotchandpur@gmail.com>
2026-06-17 04:24:26 +0600

    Fix Windows build: remove missing icons/splash/docs refs from resources.qrc and CMakeLists

--- get_logs.ps1
+++
@@ -1,9 +0,0 @@
-$run = (Invoke-RestMethod -Uri 'https://api.github.com/repos/meowgineeer/friction-aftereffetcsmod/actions/runs?per_page=1').workflow_runs[0]
-$jobs = Invoke-RestMethod -Uri $run.jobs_url
-foreach ($job in $jobs.jobs) {
-    if ($job.name -match 'Windows') {
-        Write-Host 'Job ID:' $job.id
-        Invoke-RestMethod -Uri ('https://api.github.com/repos/meowgineeer/friction-aftereffetcsmod/actions/jobs/' + $job.id + '/logs') -OutFile logs.txt
-        Select-String -Path logs.txt -Pattern 'error|fatal' -Context 2,10
-    }
-}
